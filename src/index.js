3797991b89130f1087538664793b87bb7f461d58
pranshugarg <pranshugarg02@gmail.com>
2019-10-19 22:08:56 +0530

    react-redux Provider component & connect(): mapStateToProps, mapDispatchToProps

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux'
 import reducer from './reducers'
+import { Provider } from 'react-redux'
 
 const store = createStore(reducer)
 console.log(store)
 
-ReactDOM.render(<App store = {store} />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root'));
 serviceWorker.unregister();
